ed2998874169b26ebd3d0fc31726913f704bde38
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2024-01-11 19:21:13 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { FaGithub } from "react-icons/fa";
 
 export type User = {
   id: string;
@@ -29,7 +25,6 @@ export type Repo = {
   owner: {
     avatar_url: string;
   };
-
 };
 
 const sorting = [
@@ -84,13 +79,24 @@ const App = () => {
   useEffect(() => {
     switch (sort) {
       case "stars":
-        setRepos((repos) => [...repos].sort((a, b) => b.stargazers_count - a.stargazers_count));
+        setRepos((repos) =>
+          [...repos].sort((a, b) => b.stargazers_count - a.stargazers_count)
+        );
         break;
       case "forks":
-        setRepos((repos) => [...repos].sort((a, b) => b.forks_count - a.forks_count));
+        setRepos((repos) =>
+          [...repos].sort((a, b) => b.forks_count - a.forks_count)
+        );
         break;
       case "original":
-        setRepos((repos) => [...repos].sort((a, b) => b.stargazers_count + b.forks_count - (a.stargazers_count + a.forks_count)));
+        setRepos((repos) =>
+          [...repos].sort(
+            (a, b) =>
+              b.stargazers_count +
+              b.forks_count -
+              (a.stargazers_count + a.forks_count)
+          )
+        );
         break;
       case "default":
         setRepos((repos) => [...repos].sort((a, b) => b.id - a.id));
@@ -101,64 +107,70 @@ const App = () => {
   }, [sort]);
 
   return (
-    <div className="mt-8 px-4 py-4 search-user">
-      <div>
-        <Input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          onKeyUp={handleKeyPress}
-          placeholder="Search for a user's repos"
-          className="input-search"
-        />
-        <Button
-          onClick={() => searchUsers(username)}
-          disabled={isLoading || !username}
-        >
-          {isLoading ? "Loading..." : "Search Users"}
-        </Button>
+    <>
+      <div className="header">
+        <h1>Aggregation App</h1>
+        <FaGithub className="github" />
       </div>
+      <div className="search-user">
+        <div>
+          <Input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyUp={handleKeyPress}
+            placeholder="Search for a user's repos"
+            className="input-search"
+          />
+          <Button
+            onClick={() => searchUsers(username)}
+            disabled={isLoading || !username}
+          >
+            {isLoading ? "Loading..." : "Search Users"}
+          </Button>
+        </div>
 
-      {sorting.map((sortOption) => (
-        <Button
-          key={sortOption.value}
-          onClick={() => setSort(sortOption.value)}
-          variant={sort === sortOption.value ? "default" : "outline"}
-          className={`button-sort ${
-            sort === sortOption.value ? "button-active" : ""
-          }`}
-          disabled={isLoading}
-        >
-          {sortOption.label}
-        </Button>
-      ))}
-
-      <div className="repo-list">
-        {repos.map((repo, index) => (
-          <div key={repo.id} className="repo-card">
-            <Card>
-              <div>{index + 1}</div>
-
-              <img
-                src={repo.owner.avatar_url}
-                alt="alt"
-                width={48}
-                height={48}
-              />
-              <CardHeader>
-                <CardTitle>{repo.name}</CardTitle>
-              </CardHeader>
-              <CardContent className="card-content">
-                {/* <p>{repo.</p> */}
-                <p>{repo.language ? repo.language : "Others"} </p>
-                <p>Fork Count: {repo.forks_count}</p>
-                <p>Star Count: {repo.stargazers_count}</p>
-              </CardContent>
-            </Card>
-          </div>
+        {sorting.map((sortOption) => (
+          <Button
+            key={sortOption.value}
+            onClick={() => setSort(sortOption.value)}
+            variant={sort === sortOption.value ? "default" : "outline"}
+            className={`button-sort ${
+              sort === sortOption.value ? "button-active" : ""
+            }`}
+            disabled={isLoading}
+          >
+            {sortOption.label}
+          </Button>
         ))}
+
+        <div className="repo-list">
+          {repos.map((repo, index) => (
+            <div key={repo.id} className="repo-card">
+              <Card>
+                <div>{index + 1}</div>
+
+                <img
+                  src={repo.owner.avatar_url}
+                  alt="alt"
+                  width={48}
+                  height={48}
+                />
+                <CardHeader>
+                  <CardTitle>{repo.name}</CardTitle>
+                </CardHeader>
+                <CardContent className="card-content">
+                  {/* <p>{repo.</p> */}
+                  <p>{repo.language ? repo.language : "Others"} </p>
+                  <p>Fork Count: {repo.forks_count}</p>
+                  <p>Star Count: {repo.stargazers_count}</p>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
